8020d22d45ddd7bfe28881811dbc0e5d939d0971
seohanna <alrzl4302@naver.com>
2021-12-27 11:41:30 +0900

    수정 ㅡㅡ

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Footer from '../Layouts/Footer';
-import Header from '../Layouts/Header';
+import Layout from '../Layouts/Layout';
 import Banner from '../components/Banner';
 import PartnerCompany from '../components/Partners/PartnerCompany';
 import PartnerType from '../components/Partners/PartnerType';
@@ -11,16 +10,19 @@ import Illust from '../img/sub/partnersIllust.svg';
 
 const Partners = () => {
   return (
-    <>
-    <Header />
-      <Banner visual={banner}
-        description={'핀테크를 함께 이끌어갈 다양한 분야의\n소중한 파트너를 기다립니다.'}
-        illust={Illust}
-      />
-      <PartnerType />
-      <PartnerCompany />
-    <Footer />
-    </>
+ 
+  <Layout>
+    <Banner 
+      visual={banner}
+      description={'핀테크를 함께 이끌어갈 다양한 분야의\n소중한 파트너를 기다립니다.'}
+      illust={Illust}
+    />
+    <PartnerType />
+    <PartnerCompany />
+  </Layout>
+      
+   
+
   )
 };
 
